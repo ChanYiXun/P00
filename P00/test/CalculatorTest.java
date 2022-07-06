ce8fd541cd22518eb3c1018a6944d6451925348f
ChanYiXun <21012377@myrp.edu.sg>
2022-07-06 21:37:55 +0800

    push

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -72,8 +72,19 @@ public class CalculatorTest {
 		int expected = 3;
 		assertEquals (expected, actual);
 		}
-
-
+	
+	public void testDivideWith0Denominator () { 
+		try { 
+			Calculator cal = new Calculator(); 
+			cal.divide(a , c);
+			fail("Expected an IllegalArgumentException to be thrown");
+			}catch(IllegalArgumentException e) {
+				assertEquals("Division by zero is not supported", e.getMessage() );
+			}catch(Throwable t) {
+				assertEquals("Expected an IllegalArgumentException to be thrown", t.getMessage());
+				
+			}
+	}
 		}
 	
 	
